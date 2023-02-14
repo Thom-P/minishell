2bdfb7ee923af668a932dada1a4cdcbd40b2059b
Thomas Planes <tplanes@student.42lausanne.ch>
2023-02-14 15:41:30 +0100

    added successive operator token error detection

--- source/parsing/parsing_utils.c
+++ source/parsing/parsing_utils.c
@@ -6,7 +6,7 @@
 /*   By: tplanes <tplanes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 16:11:53 by tplanes           #+#    #+#             */
-/*   Updated: 2023/02/14 15:04:09 by tplanes          ###   ########.fr       */
+/*   Updated: 2023/02/14 15:38:36 by tplanes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,30 +68,50 @@ int	odd_num_quote(char *line)
 }
 
 /* 
-Return error if:
+Returns error if:
 - first token is pipe
+- two successive tokens are operators (not that bash authorizes <>, not jmsh)
 - last token is any operator
 */
-int verify_tokens(t_list **tokens)
+int verify_tokens(t_list **ptr_tokens)
 {
+	t_list	*tokens;
 	t_tok 	*tok;
+	int		flag_op;
 
-	tok = (t_tok *)(*tokens)-> content;
+	tokens = *ptr_tokens;
+	tok = (t_tok *)tokens -> content;
 	if (tok -> type == op && tok -> str[0] == '|')
 	{	
 		printf("jmsh: syntax error near unexpected token `|'\n");
-		ft_lstclear(tokens, &free_token);
+		ft_lstclear(ptr_tokens, &free_token);
 		return (-1);
 	}
-    tok = ft_lstlast(*tokens)-> content;
-	if (tok -> type == op)
+
+	flag_op = 0;
+	while (tokens)
+	{
+		tok = (t_tok *)tokens -> content;
+		if (tok -> type == op)
+		{
+			if (flag_op == 1)
+			{
+				printf("jmsh: syntax error near unexpected token `%s'\n", tok -> str);
+				ft_lstclear(ptr_tokens, &free_token);
+				return (-1);
+			}
+			flag_op = 1;
+		}
+		else
+			flag_op = 0;
+		tokens = tokens -> next;
+	}
+	if (flag_op)
 	{	
 		printf("jmsh: syntax error near unexpected token `newline'\n");
-		ft_lstclear(tokens, &free_token);
+		ft_lstclear(ptr_tokens, &free_token);
 		return (-1);
 	}
-
-
 	return (0);
 }
 
