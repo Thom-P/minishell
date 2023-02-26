b0f0a5430fa120a29225f178f17e888aadc1d667
tplanes <tho.planes@gmail.com>
2023-02-26 12:18:54 +0100

    added some readme docs

--- source/execution/exec_line.c
+++ source/execution/exec_line.c
@@ -6,7 +6,7 @@
 /*   By: tplanes <tplanes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 15:47:32 by tplanes           #+#    #+#             */
-/*   Updated: 2023/02/24 16:08:26 by tplanes          ###   ########.fr       */
+/*   Updated: 2023/02/26 12:17:26 by tplanes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 
 void	exec_line(t_list *exec_blocks, char **envp)
 {
+	//temporary action: print the content of the exec blocks
 	(void) envp;
 	ft_lstiter(exec_blocks, &_print_block);
 	printf("\n");
+	
+	//see the readme file
+
 	return ;
 }
