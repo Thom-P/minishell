e60d6223d5685aa504ee2e22c55b66fe5ebe499c
Thomas Planes <tplanes@student.42lausanne.ch>
2023-03-01 12:08:06 +0100

    my_unset in progress

--- source/builtins/my_unset.c
+++ source/builtins/my_unset.c
@@ -6,59 +6,61 @@
 /*   By: tplanes <tplanes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 18:46:08 by tplanes           #+#    #+#             */
-/*   Updated: 2023/02/28 19:14:40 by tplanes          ###   ########.fr       */
+/*   Updated: 2023/03/01 12:06:40 by tplanes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "jmsh.h"
 
-static void	_remove_env(char *arg, char ***ptr_my_envp);
+static void	_remove_env(int ind_var, char ***ptr_my_envp);
+
+static int	_ind_var_in_env(char *var, char **my_envp);
 
 int	my_unset(int ac, char **av, char ***ptr_my_envp)
 {
 	int	exit_status;
+	int	ind_var;
 
 	exit_status = EXIT_SUCCESS;
 	if (ac == 1)
 		return (exit_status);
-	while (*av)	
+	while (*av)
 	{
-		if ((*av)[0] == '?' || (*av)[0] == '=' || (*av)[0] == '-') //MAKE SURE THAT NO EQUAL
+		ind_var = _ind_var_in_env(*av, *ptr_my_envp);
+		if (ind_in_set('=', *av) != -1 || !is_var_name_legal(*av))
 		{	
 			printf("unset: %s: not a valid identifier\n", *av);
 			exit_status = EXIT_FAILURE;
-			av++;
-			continue ;
 		}
-		if (_is_var_in_env(*av, *ptr_my_envp))
-			_add_env(*av, ptr_my_envp);
+		else if (ind_var != -1)
+			_remove_env(ind_var, ptr_my_envp);
 		av++;
 	}
 	return (exit_status);
 }
 
-static void _add_env(char *arg, char ***ptr_my_envp)
+static void _remove_env(int ind_var, char ***ptr_my_envp)
 {
 	int		new_env_size;
 	char	**my_new_envp;
-	char 	*new_var;
 	int		i;
 
-	new_env_size = get_env_size(*ptr_my_envp) + 1;
+	new_env_size = get_env_size(*ptr_my_envp) - 1;
 	my_new_envp = (char **)malloc(sizeof(char *) * (new_env_size + 1));
 	if (my_new_envp == NULL)
-		my_exit("Malloc error in export: _add_env\n", EXIT_FAILURE);
+		my_exit("Malloc error in export: _remove_env\n", EXIT_FAILURE);
 	i = 0;
-	while (i < (new_env_size - 1))
+	while (i < ind_var)
 	{	
 		my_new_envp[i] = (*ptr_my_envp)[i];
 		i++;
 	}
-	new_var = ft_strdup(arg);
-	if (new_var == NULL)
-		my_exit("Malloc error in export: strdup\n", EXIT_FAILURE);
-	my_new_envp[i++] = new_var;
-	my_new_envp[i] = NULL;
+	free((*ptr_my_envp)[i]);
+	while (i < (new_env_size - 1))
+	{	
+		my_new_envp[i] = (*ptr_my_envp)[i + 1];
+		i++;
+	}
 	free(*ptr_my_envp);
 	*ptr_my_envp = my_new_envp;
 	return ;
@@ -69,24 +71,13 @@ static int _ind_var_in_env(char *var, char **my_envp)
 	int	ind;
 	int	var_len;
 
-	ind = -1;
+	ind = 0;
 	var_len = ft_strlen(var);
-	while (*my_envp)
+	while (my_envp[ind])
 	{
-		if (ft_strncmp(*my_envp, var, var_len) == 0 && *my_envp[var_len] == '=')
-		{	
-			free(*my_envp);
-			*my_envp = ft_strdup(arg);
-			if (*my_envp == NULL)
-				my_exit("Malloc error in _repl_if_already_def\n", EXIT_FAILURE);
-			return (1);
-		}
-		my_envp++;
+		if (ft_strncmp(my_envp[ind], var, var_len) == 0 && my_envp[ind][var_len] == '=')
+			return (ind);
+		ind++;
 	}
-	return (ind);
+	return (-1);
 }
-
-/*static void	_print_export(char **my_envp);
-{
-	
-}*/
