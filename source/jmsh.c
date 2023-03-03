2190a71971f4c6840252e9454aa6b9779a4807f9
tplanes <tho.planes@gmail.com>
2023-03-03 17:01:07 +0100

    changed prompt and added parsing error code 258 to g_status

--- source/jmsh.c
+++ source/jmsh.c
@@ -6,7 +6,7 @@
 /*   By: tplanes <tplanes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 12:20:20 by tplanes           #+#    #+#             */
-/*   Updated: 2023/03/02 17:55:57 by tplanes          ###   ########.fr       */
+/*   Updated: 2023/03/03 16:59:45 by tplanes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ int	main(int ac, char **av, char **envp)
 		add_history(line);
 		if (parse_line(line, &exec_block, my_envp) == -1)
 		{
+			g_status = 258;
 			free(line);
 			continue ;		
 		}
@@ -57,17 +58,22 @@ static char	*_get_prompt(int g_status)
 {
 	char	*status;
 	char	*prompt;
+	char	*tmp;
 
 	status = ft_itoa(g_status);
 	if (status == NULL)
 		my_exit("itoa error in jmsh.c\n", EXIT_FAILURE);
 	if (g_status == 0)
-		prompt = ft_strdup("\033[1;32mjmsh-1.0$\033[0m ");
+		prompt = ft_strdup("\033[1;32mjmsh-1.0\033[0m $ ");
 	else
 	{	
-		printf("\033[1;31m");
-		prompt = ft_strjoin(status, "@jmsh-1.0$\033[0m ");
-		//prompt = ft_strjoin(status, "\033[1;31m@jmsh-1.0$\033[0m ");
+		tmp = ft_strjoin("\033[1;31m{", status);
+		if (tmp == NULL)
+			my_exit("strjoin error in jmsh.c\n", EXIT_FAILURE);
+		//printf("\033[1;31m");
+		prompt = ft_strjoin(tmp, "}jmsh-1.0\033[0m $ ");
+		free(tmp);
+		//prompt = ft_strjoin(status, "@jmsh-1.0$\033[0m ");
 	}
 	if (prompt == NULL)
 		my_exit("strjoin error in jmsh.c\n", EXIT_FAILURE);
