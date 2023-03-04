56290577708f1a0f40bc37f350375253d89cfcbe
tplanes <tho.planes@gmail.com>
2023-03-04 23:19:53 +0100

    new readline packages doesnt output ctrl chars, still need to handle ctrl-c and d

--- source/signals.c
+++ source/signals.c
@@ -6,7 +6,7 @@
 /*   By: tplanes <tplanes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 17:16:45 by tplanes           #+#    #+#             */
-/*   Updated: 2023/03/04 22:48:10 by tplanes          ###   ########.fr       */
+/*   Updated: 2023/03/04 23:17:32 by tplanes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,25 @@
 
 void	register_signals(void)
 {
+	signal(SIGINT, sigint_handler);
+	signal(SIGQUIT, SIG_IGN);
+	return ;
+}
+
+void	sigint_handler(int signum)
+{
+	(void) signum;
+	//printf("Caught a sigint!\n");
+	//printf("\n");
+	g_status = 1;
+	rl_replace_line("", 0);
+	printf("\n");
+	rl_on_new_line();
+
+	return ;
+}
+/*
 	int				fd;				
-//	int				fd2;				
-//	int				fd0;				
 	struct termios	termios_p;
 
 	fd = STDOUT_FILENO;
@@ -32,7 +48,7 @@ void	register_signals(void)
 			printf("bef: echo ctl active\n");
 		else
 			printf("bef: echo ctl inactive\n");
-		termios_p.c_lflag &= ~ECHOCTL;
+		//termios_p.c_lflag &= ~ECHOCTL;
 		int j = tcsetattr(fd, TCSANOW, &termios_p);
 		printf("j=%i\n", j);
 		if (termios_p.c_lflag & ECHOCTL)
@@ -41,59 +57,4 @@ void	register_signals(void)
 			printf("aft: echo ctl inactive\n");
 
 	}
-	/*
-	if (!isatty(fd2))
-		printf("Not a tty\n");
-	else
-	{
-		int i = tcgetattr(fd2, &termios_p);
-		printf("i=%i\n", i);
-		if (termios_p.c_lflag & ECHOCTL)
-			printf("bef: echo ctl active\n");
-		else
-			printf("bef: echo ctl inactive\n");
-		termios_p.c_lflag = termios_p.c_lflag & ~ECHOCTL;
-		int j = tcsetattr(fd2, TCSANOW, &termios_p);
-		printf("j=%i\n", j);
-		if (termios_p.c_lflag & ECHOCTL)
-			printf("aft: echo ctl active\n");
-		else
-			printf("aft: echo ctl inactive\n");
-
-	}
-
-	if (!isatty(fd0))
-		printf("Not a tty\n");
-	else
-	{
-		int i = tcgetattr(fd0, &termios_p);
-		printf("i=%i\n", i);
-		if (termios_p.c_lflag & ECHOCTL)
-			printf("bef: echo ctl active\n");
-		else
-			printf("bef: echo ctl inactive\n");
-		termios_p.c_lflag = termios_p.c_lflag & ~ECHOCTL;
-		int j = tcsetattr(fd0, TCSANOW, &termios_p);
-		printf("j=%i\n", j);
-		if (termios_p.c_lflag & ECHOCTL)
-			printf("aft: echo ctl active\n");
-		else
-			printf("aft: echo ctl inactive\n");
-
-	}
 */
-
-	signal(SIGINT, sigint_handler);
-	signal(SIGQUIT, SIG_IGN);
-	return ;
-}
-
-void	sigint_handler(int signum)
-{
-	(void) signum;
-	printf("Caught a sigint!\n");
-	g_status = 1;
-	rl_replace_line("test", 0);
-
-	return ;
-}
